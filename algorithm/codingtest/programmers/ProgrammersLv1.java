5efe8e018be7cf277b233cf41099ec257108accb
soncole <sons1998@naver.com>
2023-09-10 20:44:45 +0900

    programmers Lv.1

---
+++ algorithm/codingtest/programmers/ProgrammersLv1.java
@@ -0,0 +1,106 @@
+package algorithm.codingtest.programmers;
+
+import java.util.Arrays;
+
+public class ProgrammersLv1 {
+
+    int solution(int n) {
+        //나머지가 1이 되는 수 찾기
+        int i = 1;
+        boolean lim = n >= 3 && n <= 1000000;
+        if (lim) {
+            while (true) {
+                if (n % i == 1) {
+                    break;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return i;
+    }
+
+    int solution1(int n) {
+        //자릿수 더하기
+        int answer = 0;
+        String[] arr = String.valueOf(n).split("");
+        int[] intArr = Arrays.stream(arr)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        for (int i : intArr) {
+            answer += i;
+        }
+
+        return answer;
+    }
+
+    int solution2(int n) {
+        //약수의 합
+        int answer = 0;
+        boolean lim = n >= 0 && n <= 3000;
+        if (lim) {
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    answer += i;
+                }
+            }
+        }
+        return answer;
+    }
+
+    long[] solution3(int x, int n) {
+        //x만큼 간격이 있는 n개의 숫자
+        long[] answer = new long[n];
+        boolean lim = x >= -10000000 && x <= 10000000 && n <= 1000;
+        if (lim) {
+            for (int i = 0, j = 1; i < n; i++, j++) {
+                answer[i] = ((long) x * j);
+            }
+        }
+        return answer;
+    }
+
+    int[] solution4(long n) {
+        //자연수 뒤집어 배열로 만들기
+        StringBuffer sb = new StringBuffer();
+        String str = String.valueOf(sb.append(n).reverse());
+
+        String[] strArr = str.split("");
+
+        int[] answer = Arrays.stream(strArr)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        return answer;
+    }
+
+    boolean solution5(String s) {
+        //문자열 내 p와 y의 개수
+        String[] strArr = s.split("");
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i].contains("p") || strArr[i].contains("P")) {
+                sum1 += 1;
+            } else if (strArr[i].contains("y") || strArr[i].contains("Y")) {
+                sum2 += 1;
+            }
+        }
+        boolean answer = (sum1 == sum2) ? true : false;
+
+        return answer;
+    }
+
+    long solution6(long n) {
+        //정수 제곱근 판별
+        long answer = (long) Math.sqrt(n);
+
+        if (n == Math.pow(answer, 2)) {  //answer * answer
+            answer = (long) Math.pow(answer + 1, 2);
+        } else {
+            answer = -1;
+        }
+
+        return answer;
+    }
+}
