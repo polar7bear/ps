82d2c9af0fdc64077cc6a0a0e80f011cefbe7c87
soncole <sons1998@naver.com>
2023-10-09 01:38:25 +0900

    코드업 100제 완료

--- Main.java
+++ Main.java
@@ -1,29 +1,42 @@
 import java.io.IOException;
-import java.sql.Array;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //성실한 개미
         Scanner sc = new Scanner(System.in);
+        int[][] maze = new int[10][10];
+        int x = 1;
+        int y = 1;
 
-        int count = 0; //현재 그룹에 포함된 모험가의 수
-        int result = 0; //총 그룹의 수
-        int n = sc.nextInt();   //모험가 수 입력
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        for(int i = 0; i < n; i++) {
-            arrayList.add(sc.nextInt());    //모험가 수에 따른 각 공포도 입력
-        }
-        Collections.sort(arrayList);    //오름차순 정렬
-
-        for(int i = 0; i < n; i++) {
-            count += 1;     //현재 그룹에 해당 모험가를 포함시키기
-            if(count >= arrayList.get(i)) {
-                result += 1;    // 총 그룹의 수 증가시키기
-                count = 0;  //현재 그룹에 포함된 모험가의 수 초기화
+        for(int i = 0; i < maze.length; i++) {
+            for(int j = 0; j < maze[i].length; j++) {
+                maze[i][j] = sc.nextInt();
             }
         }
-        System.out.println(result);
+
+        while(true) {
+            if(maze[x][y] == 2) {
+                maze[x][y] = 9;
+                break;
+            }
+            maze[x][y] = 9;
+            if(maze[x][y+1] == 0 || maze[x][y+1] == 2) {
+                y++;
+                continue;
+            } else if(maze[x][y+1] == 1) {
+                if(maze[x+1][y] == 0 || maze[x+1][y] == 2) {
+                    x++;
+                    continue;
+                } else if(maze[x+1][y] == 1) break;
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
--- algorithm/codingtest/codeup/CodeUp1.java
+++ algorithm/codingtest/codeup/CodeUp1.java
@@ -1013,4 +1013,72 @@ public class CodeUp1 {
             System.out.println();
         }
     }
+
+    public void codeUp1098() throws IOException {
+        //설탕과자 뽑기
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();   //세로길이
+        int w = sc.nextInt();   //가로길이
+
+        int n = sc.nextInt(); //막대의 개수
+        int[][] square = new int[h][w];
+
+        for (int i = 0; i < n; i++) {
+            int l = sc.nextInt(); //길이
+            int d = sc.nextInt(); //방향
+            int x = sc.nextInt(); //x좌표
+            int y = sc.nextInt(); //y좌표
+            for (int j = 0; j < l; j++) {
+                if (d == 0) {
+                    square[x - 1][y - 1 + j] = 1;
+                } else if (d == 1) {
+                    square[x - 1 + j][y - 1] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                System.out.print(square[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void codeUp1099() throws IOException {
+        //성실한 개미
+        Scanner sc = new Scanner(System.in);
+        int[][] maze = new int[10][10];
+        int x = 1;
+        int y = 1;
+
+        for(int i = 0; i < maze.length; i++) {
+            for(int j = 0; j < maze[i].length; j++) {
+                maze[i][j] = sc.nextInt();
+            }
+        }
+
+        while(true) {
+            if(maze[x][y] == 2) {
+                maze[x][y] = 9;
+                break;
+            }
+            maze[x][y] = 9;
+            if(maze[x][y+1] == 0 || maze[x][y+1] == 2) {
+                y++;
+                continue;
+            } else if(maze[x][y+1] == 1) {
+                if(maze[x+1][y] == 0 || maze[x+1][y] == 2) {
+                    x++;
+                    continue;
+                } else if(maze[x+1][y] == 1) break;
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
