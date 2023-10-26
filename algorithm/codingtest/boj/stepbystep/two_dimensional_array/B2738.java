ecc8dd194d211c648268f739a4d059ade6f08deb
solarbear <sons1998@naver.com>
2023-10-26 18:07:56 +0900

    행렬의 덧셈

--- algorithm/codingtest/boj/stepbystep/two_dimensional_array/B2738.java
+++ algorithm/codingtest/boj/stepbystep/two_dimensional_array/B2738.java
@@ -1,8 +1,6 @@
 package algorithm.codingtest.boj.stepbystep.two_dimensional_array;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class B2738 {
