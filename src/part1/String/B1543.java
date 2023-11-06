978f8993072885800bcf53162f1b0e90dd01c361
solarbear <sons1998@naver.com>
2023-11-06 19:32:59 +0900

    문서 검색

---
+++ src/part1/String/B1543.java
@@ -0,0 +1,57 @@
+package part1.String;
+
+import java.util.Scanner;
+
+public class B1543 {
+    public static void main(String[] args) {
+        //문서 검색
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String input = sc.nextLine();
+        int res = 0;
+
+        for(int i = 0 ; i < str.length(); i++) {
+            boolean isMatch = true;
+            for (int j = 0; j < input.length(); j++) {
+                if(i + j >= str.length() || str.charAt(i+j) != input.charAt(j)) {
+                    isMatch = false;
+                    break;
+                }
+            }
+            if(isMatch) {
+                res++;
+                i += input.length() - 1;
+            }
+        }
+        System.out.println(res);
+
+        //indexOf 사용
+        /*Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String input = sc.nextLine();
+        int res = 0;
+        int start = 0;
+
+        while(true) {
+            int idx = str.indexOf(input, start);
+
+            if(idx < 0) break;
+            start = idx + input.length();
+            res++;
+        }
+        System.out.println(res);*/
+
+        //replace 사용
+        /*Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String input = sc.nextLine();
+
+        String replaceStr = str.replace(input, "");
+        int length = str.length() - replaceStr.length();
+        int res = length / input.length();
+        System.out.println(res);*/
+    }
+}
