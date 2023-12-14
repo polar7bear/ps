ae360c8b64ff812568470b9ced98c8d891db128f
polar7bear <124570553+polar7bear@users.noreply.github.com>
2023-12-14 22:49:02 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2396 ms, Memory: 140884 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i : arr) bw.write(i + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
+
