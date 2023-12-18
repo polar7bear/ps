274f17565d1a1cf17404f65c6580fc06fb142f10
polar7bear <124570553+polar7bear@users.noreply.github.com>
2023-12-18 18:30:42 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 316 ms, Memory: 27824 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); // 굴다리의 길이
+        int m = Integer.parseInt(br.readLine()); // 가로등의 개수
+        ArrayList<Integer> lights = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < m; i++) {
+            lights.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(lights);
+
+        int maxDistance = Math.max(lights.get(0), n - lights.get(m - 1)); // 시작과 끝 지점 처리
+        for (int i = 1; i < lights.size(); i++) {
+            maxDistance = Math.max(maxDistance, (lights.get(i) - lights.get(i - 1) + 1) / 2); // 인접 가로등 사이 거리
+        }
+
+        System.out.println(maxDistance);
+    }
+}
