6ce77e35939517122614fb5e28233e71e2858f8b
polar7bear <124570553+polar7bear@users.noreply.github.com>
2023-12-18 21:16:53 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 300 ms, Memory: 25016 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -1,27 +1,46 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine()); // 굴다리의 길이
-        int m = Integer.parseInt(br.readLine()); // 가로등의 개수
-        ArrayList<Integer> lights = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine()); //도로의 길이
+        int m = Integer.parseInt(br.readLine()); //가로등의 개수
+        int[] lights = new int[m];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            lights.add(Integer.parseInt(st.nextToken()));
+            lights[i] = Integer.parseInt(st.nextToken());
         }
 
-        int maxDistance = Math.max(lights.get(0), n - lights.get(m - 1)); // 시작과 끝 지점 처리
-        for (int i = 1; i < lights.size(); i++) {
-            maxDistance = Math.max(maxDistance, (lights.get(i) - lights.get(i - 1) + 1) / 2); // 인접 가로등 사이 거리
-        }
+        System.out.println(binarySearch(n, lights));
+    }
 
-        System.out.println(maxDistance);
+    public static int binarySearch(int n, int[] lights) {
+        int left = 0, right = n;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isFull(n, lights, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static boolean isFull(int n, int[] lights, int range) {
+        int last = 0;
+        for (int light : lights) {
+            if (light - range > last) {
+                return false;
+            }
+            last = Math.max(last, light + range);
+        }
+        return last >= n;
     }
 }
