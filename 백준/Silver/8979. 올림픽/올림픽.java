e09322395946e65c1be730cbe516a5557885c0a2
polar7bear <124570553+polar7bear@users.noreply.github.com>
2023-12-22 00:10:18 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14132 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); //국가의 개수
+        int k = Integer.parseInt(st.nextToken()); //등수를 알고싶은 국가
+
+        int rank = 1;
+
+        List<Country> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int id = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+            list.add(new Country(id, gold, silver, bronze));
+        }
+
+        Collections.sort(list);
+
+        for(int i = 1; i < list.size(); i++) {
+            if(list.get(i-1).compareTo(list.get(i)) != 0) {
+                rank = i + 1;
+            }
+            if(list.get(i).id == k) {
+                System.out.println(rank);
+                break;
+            }
+        }
+    }
+
+    static class Country implements Comparable<Country> {
+        int id, gold, silver, bronze;
+
+        public Country(int id, int gold, int silver, int bronze) {
+            this.id = id;
+            this.gold = gold;
+            this.silver = silver;
+            this.bronze = bronze;
+        }
+
+        @Override
+        public int compareTo(Country o) {
+            if(gold != o.gold) {
+                return o.gold - gold;
+            }
+            else if(silver != o.silver) {
+                return o.silver - silver;
+            } else {
+                return o.bronze - bronze;
+            }
+        }
+    }
+}
+
