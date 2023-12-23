6dc4a2b555c7e8a304e23178cba5d8ac83cbbec3
polar7bear <124570553+polar7bear@users.noreply.github.com>
2023-12-23 22:45:34 +0900

    [Gold V] Title: 토마토, Time: 624 ms, Memory: 119632 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int m, n;
+    static int[][] tomato;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static Queue<int[]> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        tomato = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                tomato[i][j] = Integer.parseInt(st.nextToken());
+                if (tomato[i][j] == 1) q.add(new int[]{i, j});
+            }
+        }
+
+        System.out.println(bfs());
+    }
+
+    private static int bfs() {
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+
+                if (nx < 0 || n <= nx || ny < 0 || m <= ny || tomato[nx][ny] != 0) continue;
+
+                tomato[nx][ny] = tomato[now[0]][now[1]] + 1;
+                q.add(new int[]{nx, ny});
+            }
+        }
+
+        int day = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tomato[i][j] == 0) {
+                    return -1;
+                }
+                day = Math.max(day, tomato[i][j]);
+            }
+        }
+        return day - 1;
+    }
+}
+
+
+
