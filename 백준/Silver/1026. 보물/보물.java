0f86ea0ace92cc3895485f0c6e696ecbe64e2cf9
polar7bear <124570553+polar7bear@users.noreply.github.com>
2023-12-28 22:08:06 +0900

    [Silver IV] Title: 보물, Time: 128 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,12 +9,31 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         Integer[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
         Integer[] b = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+        boolean[] usedA = new boolean[n];
+        boolean[] usedB = new boolean[n];
 
         int res = 0;
-        Arrays.sort(a);
-        Arrays.sort(b, Collections.reverseOrder());
         for (int i = 0; i < n; i++) {
-            res += a[i] * b[i];
+
+            int min = -1;
+            for (int j = 0; j < n; j++) {
+                if (!usedA[j] && (min == -1 || a[j] < a[min])) {
+                    min = j;
+                }
+            }
+
+            int max = -1;
+            int idx = -1;
+            for (int j = 0; j < n; j++) {
+                if (!usedB[j] && (idx == -1 || b[j] > max)) {
+                    max = b[j];
+                    idx = j;
+                }
+            }
+
+            res += a[min] * max;
+            usedA[min] = true;
+            usedB[idx] = true;
         }
         System.out.println(res);
     }
