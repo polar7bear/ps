b527805bd81c73bcc342bf6bddf2ceb43251331d
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-08-15 18:50:30 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13904 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|").append("\n")
+                .append("|q p|   /}").append("\n")
+                .append("( 0 )\"\"\"\\").append("\n")
+                .append("|\"^\"`    |").append("\n")
+                .append("||_/=\\\\__|");
+
+        System.out.println(sb);
+    }
+}
