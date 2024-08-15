e4476c26b2a7ff0fc3a4daa7b665bd2fd1013ff3
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-08-15 16:27:06 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13976 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append("\n")
+                .append(" )  ( ')").append("\n")
+                .append("(  /  )").append("\n")
+                .append(" \\(__)|");
+        System.out.println(sb);
+    }
+}
