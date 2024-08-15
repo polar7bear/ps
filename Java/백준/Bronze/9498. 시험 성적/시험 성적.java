a1b70476ea381e6b91a1b54f0f8eb2f3bbb6e067
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-08-15 18:45:35 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- Java/백준/Bronze/9498. 시험 성적/시험 성적.java
+++ Java/백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,24 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int score = Integer.parseInt(br.readLine());
-
-        String res = Stream.of(score)
-                .map(s -> {
-                    if (s >= 90) return "A";
-                    else if (s >= 80) return "B";
-                    else if (s >= 70) return "C";
-                    else if (s >= 60) return "D";
-                    else return "F";
-                })
-                .findFirst()
-                .get();
-
-        System.out.println(res);
+        System.out.println(score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : score >= 60 ? "D" : "F");
     }
 }
