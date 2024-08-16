c7f5bd0bad8395a6f773c1475061032f08f85560
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-08-16 12:08:14 +0900

    [Bronze I] Title: 팰린드롬수, Time: 92 ms, Memory: 13968 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("0")) break;
+
+            System.out.println(isPalindrome(input) ? "yes" : "no");
+        }
+    }
+
+    static boolean isPalindrome(String input) {
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
