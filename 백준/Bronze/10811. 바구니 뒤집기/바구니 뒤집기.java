8a85c914db643d0e92e12e875c8cdf95e02983d7
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-09-04 17:21:30 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 116 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) arr[i] = i;
+
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+
+            while (x < y) {
+                int tmp = arr[x];
+                arr[x] = arr[y];
+                arr[y] = tmp;
+                x++;
+                y--;
+            }
+        }
+        for (int i = 1; i <= n; i++) System.out.print(arr[i] + " ");
+    }
+}
