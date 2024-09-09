6af5d76983aa02c0bf798d9de96cd76661240eb0
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-09-09 18:47:39 +0900

    [Silver II] Title: 최소 힙, Time: 276 ms, Memory: 26288 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -6,15 +6,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+
         while (n-- > 0) {
             int x = Integer.parseInt(br.readLine());
             if (x == 0 && pq.isEmpty()) {
-                System.out.println(0);
+                sb.append(0).append("\n");
             } else if (x == 0) {
-                System.out.println(pq.poll());
+                sb.append(pq.poll()).append("\n");
             } else {
                 pq.add(x);
             }
         }
+        System.out.println(sb);
     }
 }
