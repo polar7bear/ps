3252affc7212ca406aa8e60b2f5330f80d170f64
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-09-10 15:08:15 +0900

    [Silver II] Title: 나무 자르기, Time: 540 ms, Memory: 119508 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int left = 0, right = Arrays.stream(arr).max().getAsInt();
+        int res = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long total = 0;
+
+            for (int tree : arr) {
+                if (tree > mid) total += tree - mid;
+            }
+
+            if (total >= m) {
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(res);
+    }
+}
