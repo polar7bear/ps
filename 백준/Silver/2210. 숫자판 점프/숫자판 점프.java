23ccc409655fa43e29566beeea124272434b88a7
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-09-27 11:26:29 +0900

    [Silver II] Title: 숫자판 점프, Time: 160 ms, Memory: 19048 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] graph = new int[5][5];
+    static Set<String> resultSet = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        for (int i = 0; i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                dfs(i, j, 0, "");
+            }
+        }
+        System.out.println(resultSet.size());
+    }
+
+    static void dfs(int x, int y, int depth, String str) {
+        if (depth == 6) {
+            resultSet.add(str);
+            return;
+        }
+        str += graph[x][y];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx > 4 || ny < 0 || ny > 4) continue;
+
+            dfs(nx, ny, depth + 1, str);
+        }
+    }
+}
