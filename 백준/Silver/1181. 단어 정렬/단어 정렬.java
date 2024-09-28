edb6a2c114c439d86a95c2b2fe7844e60e1f3c39
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-09-28 11:47:48 +0900

    [Silver V] Title: 단어 정렬, Time: 288 ms, Memory: 26476 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,34 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         String[] arr = new String[n];
 
-        for(int i = 0; i < n; i++) {
-            arr[i] = br.readLine();
-        }
+        for (int i = 0; i < n; i++) arr[i] = br.readLine();
 
         Arrays.sort(arr, (o1, o2) -> {
-            if(o1.length() == o2.length()) return o1.compareTo(o2);
+            if (o1.length() == o2.length()) return o1.compareTo(o2);
             return o1.length() - o2.length();
         });
-
         Set<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
 
-        for(String s : arr) {
-            if(!set.contains(s)) {
-                System.out.println(s);
+        for (String s : arr) {
+            if (!set.contains(s)) {
                 set.add(s);
+                sb.append(s).append("\n");
             }
         }
+        System.out.println(sb);
     }
 }
