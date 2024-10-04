7aede7c36af898a962429350efa025ad71d2c806
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-10-04 12:48:44 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,16 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*
+        1,2,3 번째는 1 고정
+        4번째(2) = 첫번째(1) + 세번쨰(1)
+        5번째(2) = 4번째(2)
+        6번째(3) = 5번쨰(2) + 첫번째(1) -> 이때부터 규칙 생김
+        11번째(12) = 10번째(9) + 6번째(3)
+        12번째(16) = 11번째(12) + 7번째(4)
+        13번째(21) = 12번쩨(16) + 8번째(5)
+        14번째 = 13번째 + 9번째*/
         int t = Integer.parseInt(br.readLine());
-
         long[] dp = new long[101];
         dp[1] = dp[2] = dp[3] = 1;
-        for (int i = 4; i <= 100; i++) {
-            dp[i] = dp[i - 3] + dp[i - 2];
+        dp[4] = dp[5] = 2;
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
         }
 
         while (t-- > 0) {
