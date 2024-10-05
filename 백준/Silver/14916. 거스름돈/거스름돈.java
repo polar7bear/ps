a9ac05f1378a9682c199374f66f6f148e0c356bb
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-10-05 09:20:45 +0900

    [Silver V] Title: 거스름돈, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        while (n >= 0) {
+            if (n % 5 == 0) {
+                cnt += n / 5;
+                System.out.println(cnt);
+                return;
+            } else if (n == 1 || n == 3) {
+                System.out.println(-1);
+                return;
+            }
+            n -= 2;
+            cnt++;
+        }
+    }
+}
