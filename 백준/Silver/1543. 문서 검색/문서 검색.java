465a0f269c10d5509d45807ad42ce655b1a5190e
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-10-08 09:15:57 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String find = br.readLine();
+
+        String replace = input.replace(find, "");
+        System.out.println((input.length() - replace.length()) / find.length());
+    }
+}
