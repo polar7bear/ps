3f069e0645805f2ec26cae88ff9c5be490954630
polar7bear <124570553+polar7bear@users.noreply.github.com>
2024-10-11 23:28:47 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 116 ms, Memory: 15504 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        System.out.println(s.substring(s.length() - 5));
+    }
+}
