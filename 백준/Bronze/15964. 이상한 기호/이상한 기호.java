20ccc2e0e3093f1089765bcfc5e1b99fd1da4a47
polar7bear <124570553+polar7bear@users.noreply.github.com>
2026-01-31 13:39:06 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14192 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int res = (a + b) * (a - b);
+        System.out.println(res);
+    }
+}
